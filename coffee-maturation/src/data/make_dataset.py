c37aaff7cdd5ae23cdf3d0252610784a4b0faff2
AngellyO <oangelly@uninorte.edu.co>
2021-02-04 23:48:12 -0500

    function dataset

--- coffee-maturation/src/data/make_dataset.py
+++ coffee-maturation/src/data/make_dataset.py
@@ -19,41 +19,48 @@ from sklearn.preprocessing import MultiLabelBinarizer
 #	help="path to output accuracy/loss plot")
 #args = vars(ap.parse_args())
 
-# initialize the number of epochs to train for, initial learning rate,
-# batch size, and image dimensions
-IMAGE_DIMS = (70, 70, 3)
-
-# grab the image paths and randomly shuffle them
-print("[INFO] loading images...")
-path_input = "././data/raw/"
-imagePaths = sorted(list(paths.list_images(path_input)))
-random.seed(42)
-random.shuffle(imagePaths)
-
-# initialize the data and labels
-X = []
-labels = []
-
-# loop over the input images
-for imagePath in imagePaths:
-	# load the image, pre-process it, and store it in the data list
-	image = cv2.imread(imagePath)
-	image = cv2.resize(image, (IMAGE_DIMS[1], IMAGE_DIMS[0]))
-	image = img_to_array(image)
-	X.append(image)
-	# extract set of class labels from the image path and update the
-	# labels list
-	l = imagePath.split(os.path.sep)[-2].split("-")
-	l = tuple(l)
-	labels.append(l)
+def create_dataset():
+	"""
+	This function returns a tuple X, y
+	"""
 	
-mlb = MultiLabelBinarizer()
-mlb.fit(labels)
-y = mlb.transform(labels)
-
-# save the multi-label binarizer to disk
-print("[INFO] serializing label binarizer...")
-path_output = "././data/processed/"
-f = open(path_output + "mlb.pickle", "wb")
-f.write(pickle.dumps(mlb))
-f.close()
+	# initialize the number of epochs to train for, initial learning rate,
+	# batch size, and image dimensions
+	IMAGE_DIMS = (70, 70, 3)
+	
+	# grab the image paths and randomly shuffle them
+	print("[INFO] loading images...")
+	path_input = "././data/raw/"
+	imagePaths = sorted(list(paths.list_images(path_input)))
+	random.seed(42)
+	random.shuffle(imagePaths)
+	
+	# initialize the data and labels
+	X = []
+	labels = []
+	
+	# loop over the input images
+	for imagePath in imagePaths:
+		# load the image, pre-process it, and store it in the data list
+		image = cv2.imread(imagePath)
+		image = cv2.resize(image, (IMAGE_DIMS[1], IMAGE_DIMS[0]))
+		image = img_to_array(image)
+		X.append(image)
+		# extract set of class labels from the image path and update the
+		# labels list
+		l = imagePath.split(os.path.sep)[-2].split("-")
+		l = tuple(l)
+		labels.append(l)
+		
+	mlb = MultiLabelBinarizer()
+	mlb.fit(labels)
+	y = mlb.transform(labels)
+	
+	# save the multi-label binarizer to disk
+	print("[INFO] serializing label binarizer...")
+	path_output = "././data/processed/"
+	f = open(path_output + "mlb.pickle", "wb")
+	f.write(pickle.dumps(mlb))
+	f.close()
+	
+	return X, y
