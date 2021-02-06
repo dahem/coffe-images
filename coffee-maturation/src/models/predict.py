5fa76233c9224a9197226adf67537d30357e04e1
Nury Yanque <nury.yanque@cromai.com>
2021-02-06 00:23:12 -0300

    Fix predict on each sliding window

--- coffee-maturation/src/models/predict.py
+++ coffee-maturation/src/models/predict.py
@@ -12,11 +12,11 @@ import src.models.sliding_window as slidewin
 
 # load the trained convolutional neural network and the multi-label
 # binarizer
-# print("[INFO] loading network...")
-# out_model = "././models/coffematuration.h5"
-# model = load_model(out_model)
-# path_labelbin = "././models/mlb.pickle"
-# mlb = pickle.loads(open(path_labelbin, "rb").read())
+print("[INFO] loading network...")
+out_model = "././models/coffematuration.h5"
+model = load_model(out_model)
+path_labelbin = "././models/mlb.pickle"
+mlb = pickle.loads(open(path_labelbin, "rb").read())
 
 step = 70
 path_img_val = "././data/raw/validation/cafe700.jpg"
@@ -26,25 +26,29 @@ w, h = image.shape[0], image.shape[1]
 print("IMAGE: ", path_img_val)
 print(w, h)
 
-img_copy = (image/256)*255
-img_copy = np.array(img_copy, dtype='uint8')
+img_copy = np.array(image, dtype='uint8')
 
 coord = []
 
 for (x, y, roi) in slidewin.sliding_window(img_copy, int(step), (w, h)):
     print(x, y)
-    print("roi shape: ", roi.shape)
-    if roi.shape[1] != w or roi.shape[0] != h:
-            continue
+    # print("roi shape: ", roi.shape)
+    # if roi.shape[1] != w or roi.shape[0] != h:
+    #         continue
     rx, ry, rx2, ry2 = int(x), int(y), int((x+w)), int((y+h))
     # coord.append([rx,ry,rx2,ry2,roi])
 
-    roi = roi.astype("float") / 255.0
-    roi = img_to_array(roi)
-    
-#     roi = np.expand_dims(roi, axis=0)
+    if roi.shape == (step,step,3):
+        roi = roi.astype("float") / 255.0
+        roi = img_to_array(roi)
+        
+        roi = np.expand_dims(roi, axis=0)
 
-#     classes_prob = model.predict(roi)[0]
-#     idxs = np.argsort(classes_prob)[::-1][:2]
+        
+        classes_prob = model.predict(roi)[0]
+        # idxs = np.argsort(classes_prob)[::-1][:2]
+        print(classes_prob)
+    else:
+        continue
 
     
--- coffee-maturation/src/models/sliding_window.py
+++ coffee-maturation/src/models/sliding_window.py
@@ -1,8 +1,8 @@
 def sliding_window(image, stepSize, windowSize):
 	# print("\n image:"+ str(image))
 	print("\n step:"+ str(stepSize))
-	for y in range(0, image.shape[0], stepSize):
-		for x in range(0, image.shape[1], stepSize):
-			print(y, x, "image[", y, y + stepSize, x, x + stepSize,"]")
-            # yield (y, x, image[y:y + windowSize[1], x:x + windowSize[0]])
+	for x in range(0, image.shape[0], stepSize):
+		for y in range(0, image.shape[1], stepSize):
+			yield (x, y, image[x:x + stepSize, y:y + stepSize])
+            # print(y, x, "image[", y, y + stepSize, x, x + stepSize,"]")
             
